d597cd514775cfb3d6c807ec50513b1bf0d6baf1
Vasyl Deresh <81803610+DeDKiK@users.noreply.github.com>
2023-04-13 12:06:05 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,83 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println(" Lab 3");
-	// write your code here
+	public class Main {
+    static class employee {
+        String name;
+        String surname;
+        int age;
+        String post;
+
+        public void Show() {
+            System.out.println("Ім'я: " + name);
+            System.out.println("Прізвище: " + surname);
+            System.out.println("Вік: " + age);
+            System.out.println("Посада: " + post);
+        }
+    }
+
+    static class Person extends employee {
+        String sex;
+
+        public void Show() {
+            super.Show();
+            System.out.println("Стать: " + sex);
+        }
+    }
+
+    static class Worker extends employee {
+        int payment;
+
+        public void Show() {
+            super.Show();
+            System.out.println("Зарплата: " + payment);
+        }
+    }
+
+    static class Engenier extends employee {
+        String Specialization;
+
+        public void Show() {
+            super.Show();
+            System.out.println("Спеціалізація: " + Specialization);
+        }
+    }
+
+    public static void main(String[] args) {
+        employee[] employee = new employee[3];
+
+        Person Person = new Person();
+        Person.name = "Іван";
+        Person.surname = "Іванов";
+        Person.age = 30;
+        Person.post = "Менеджер";
+        Person.sex = "Чоловіча";
+        Person.Show();
+        System.out.println(" ");
+
+        Worker Worker = new Worker();
+        Worker.name = "Олександра";
+        Worker.surname = "Петренко";
+        Worker.age = 25;
+        Worker.post = "Офіціант";
+        Worker.payment = 10000;
+        Worker.Show();
+        System.out.println(" ");
+
+        Engenier Engenier = new Engenier();
+        Engenier.name = "Максим";
+        Engenier.surname = "Сидоренко";
+        Engenier.age = 35;
+        Engenier.post = "Інженер";
+        Engenier.Specialization = "Електротехніка";
+        Engenier.Show();
+    }
+}
+	    
+	    
+
+	    
+	    
+	    
     }
 }
